762af9b52d349f87a8e97cf317f086a1f2e1cc8d
John Laubscher <jlmemphiscommanders@gmail.com>
2021-06-04 17:06:10 -0500

    mvp done

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -6,12 +6,42 @@ import App from "../App";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
-render(<App />);
+test("form header renders", () => {
+  render(<App />);
 
-userEvent.click(screen.getByText(/cart/i));
-userEvent.click(screen.getByText(/checkout/i));
+  userEvent.click(screen.getByText(/cart/i));
+  userEvent.click(screen.getByText(/checkout/i));
 
-expect(screen.getByText(/react plants/i));
+  expect(screen.getByText(/react plants/i));
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm />);
+  // Navigating to checkout form
+  //   userEvent.click(screen.getByText(/cart/i));
+  //   const checkoutButton = screen.getByText(/checkout/i);
+  //   userEvent.click(checkoutButton);
+  // Filling out checkout form
+  const firstNameText = screen.getByLabelText("First Name:");
+  userEvent.type(firstNameText, "Lance");
+
+  const lastNameText = screen.getByLabelText("Last Name:");
+  userEvent.type(lastNameText, "DragonTrainer");
+
+  const AddressText = screen.getByLabelText("Address:");
+  userEvent.type(AddressText, "123 FinalFour");
+
+  const cityText = screen.getByLabelText("City:");
+  userEvent.type(cityText, "FinalFour city");
+
+  const stateText = screen.getByLabelText("State:");
+  userEvent.type(stateText, "FinalFour state");
+
+  const zipText = screen.getByLabelText("Zip:");
+  userEvent.type(zipText, "38122");
+  //Submitting Checkout Form
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+  //Checking to see if success message appears
+  expect(screen.getByTestId("successMessage"));
+});
